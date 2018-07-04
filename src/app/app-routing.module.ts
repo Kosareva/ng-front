ed7a97965d83175826e7134409a6d4045ca8bdfd
Lizaveta <kosareva962@gmail.com>
2018-07-04 16:00:41 +0300

    styles and header added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import {NgModule} from "@angular/core";
 import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
-// import {HomeComponent} from "./core/home/home.component";
 
 const appRoutes: Routes = [
-    // {path: '', component: HomeComponent},
     {path: '', loadChildren: './core/core.module#CoreModule'},
     {path: 'contact-us', loadChildren: './contact-us/contact-us.module#ContactUsModule'}
 ];
